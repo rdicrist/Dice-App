54236ca3101b7c3a0031955d8f71d3a59b555640
Ricki DiCristoforo <rdicrist@gmail.com>
2022-04-12 14:22:54 -0700

    Initial Commit

---
+++ dice/diceApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceApp.swift
+//  dice
+//
+//  Created by Ricki DiCristoforo on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct diceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
